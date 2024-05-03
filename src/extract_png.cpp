c6000b002564236e2acbfd4e772fd7e0756a01f4
Padmavati-123 <162453633+Padmavati-123@users.noreply.github.com>
2024-05-03 20:04:26 +0530

    Updated extract_png.cpp

--- src/extract_png.cpp
+++ src/extract_png.cpp
@@ -2,35 +2,41 @@
 
 void stego::extract_png() {
     // Implement extraction of message from png file here
-    bool encryption_bit = 0;
-    bool compression_bit = 0;
+     encryption_bit = 0;
+     compression_bit = 0;
     
     size_t currentindex = 8;
+    string input_str = input_buffer.str();
 
-    while (currentindex < input_buffer.str().size()) 
+    while (currentindex < input_str.size()) 
     {
         size_t chunkSize = 
-                (input_buffer.str()[currentindex] << 24) |
-                (input_buffer.str()[currentindex + 1] << 16) |
-                (input_buffer.str()[currentindex + 2] << 8) |
-                (input_buffer.str()[currentindex + 3]);
+                (input_str[currentindex] << 24) |
+                (input_str[currentindex + 1] << 16) |
+                (input_str[currentindex + 2] << 8) |
+                (input_str[currentindex + 3]);
         currentindex += 4;
 
-        if (input_buffer.str()[currentindex] == 'I' && input_buffer.str()[currentindex + 1] == 'D' &&
-                input_buffer.str()[currentindex + 2] == 'A' && input_buffer.str()[currentindex + 3] == 'T')
+        if (input_str[currentindex] == 'I' && input_str[currentindex + 1] == 'D' &&
+                input_str[currentindex + 2] == 'A' && input_str[currentindex + 3] == 'T')
         {
             currentindex += 4;
 
-            encryption_bit = input_buffer.str()[currentindex] & 0x01;
-            compression_bit = input_buffer.str()[currentindex] & 0x02;
-            currentindex += 17;
+            encryption_bit = input_str[currentindex] & 0x01;
+            compression_bit = input_str[currentindex] & 0x02;
 
-            size_t messageSize =
-                (input_buffer.str()[currentindex] << 24) |
-                (input_buffer.str()[currentindex + 1] << 16) |
-                (input_buffer.str()[currentindex + 2] << 8) |
-                (input_buffer.str()[currentindex + 3]);
-            currentindex += 4;
+            uint32_t messageSize = 0;
+            for (int i = 0; i < 4; i++) 
+            {
+                uint8_t message_size_byte = 
+                ((input_str[currentindex] & 0x03) << 6) |
+                ((input_str[currentindex + 1] & 0x03) << 4) |
+                ((input_str[currentindex + 2] & 0x03)<< 2) |
+                ((input_str[currentindex + 3] & 0x03));
+                
+
+                messageSize |= message_size_byte << (8 * (3-i));
+                currentindex += 4;
 
             for (size_t i = 0; i < messageSize; ++i)
             {
@@ -38,7 +44,7 @@ void stego::extract_png() {
 
                 for (int j = 0; j < 4; ++j) 
                 {
-                    unsigned char pattern = input_buffer.str()[currentindex + j] & 0x03;
+                    unsigned char pattern = input_str[currentindex + j] & 0x03;
                     messageByte |= pattern;
                     if (j < 3)
                     {
@@ -51,10 +57,11 @@ void stego::extract_png() {
                 message_buffer.put(messageByte);
             }
             break;
-        } 
+          } 
+        }
         else 
         {
-            currentindex += chunkSize + 4;
+            currentindex += (chunkSize + 8);
         }
     }
 }
